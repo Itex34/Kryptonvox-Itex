044cd24996d0f2764349fdccaa21f7d86af26329
Itex34 <83979354+Itex34@users.noreply.github.com>
2026-01-16 12:28:19 +0100

    Add files via upload

---
+++ source code/config.js
@@ -0,0 +1,16 @@
+export const CONFIG = Object.freeze({
+  SKIN_URLS: {
+    DEFAULT: 'https://voxiom.io/package/cb1d14c1ff0efb6a282b.png',
+    RUBY: 'https://voxiom.io/package/aef55bdd0c3c3c3734f8.png',
+    SAPPHIRE: 'https://voxiom.io/package/ecca1227c2e0406be225.png'
+  },
+  COLORS: {
+    default: { head: '#24b44d', body: '#ee1c23' },
+    ruby: { head: '#ffffff', body: '#ee1c23' },
+    sapphire: { head: '#ffffff', body: '#1919ff' }
+  },
+  DEFAULT_KEYBINDS: {
+    FOCUS: { key: 'k', modifier: 'alt' },
+    CONTAINER: { key: 'z', modifier: 'alt' }
+  }
+});
